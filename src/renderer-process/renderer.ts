0e37ac76197925baf2df17a9b1615edbf97eb503
kosuke <kou.shinto@icloud.com>
2019-12-01 22:43:52 +0900

    notice message

--- src/renderer-process/renderer.ts
+++ src/renderer-process/renderer.ts
@@ -1,26 +1,32 @@
-import { remote, ipcRenderer, Remote, BrowserWindow } from "electron"
+import { remote, ipcRenderer, BrowserWindow } from "electron"
 import * as path from "path"
 
 
 const genbtn = document.getElementById('genbtn')
-
 let win:BrowserWindow
 
 genbtn.addEventListener('click', (event) => {
 	const modalPath: string = path.resolve(__dirname, `../../src/modal-window.html`)
 	let url = (<HTMLInputElement>document.getElementById('url')).value
-	// ipcRenderer.sendSync('qrcode', url)
 
-	win = new remote.BrowserWindow ({
-		width: 400,
-		height: 320,
-		titleBarStyle: "hidden",
-	})
-
-
-	win.on('close', () => { win = null })
-	win.loadFile(modalPath)
-	win.show()
+	if (url !== '') {
+		// ipcRenderer.sendSync('qrcode', url)
+	
+		win = new remote.BrowserWindow ({
+			width: 400,
+			height: 320,
+			titleBarStyle: "hidden",
+		})
+	
+		win.on('close', () => { win = null })
+		win.loadFile(modalPath)
+		win.show()
+	} else {
+		// テキストが入力されていない場合、エラーメッセージを表示
+		const message: string = 'Error! URL is blank.'
+		let el = (<HTMLInputElement>document.getElementById('notice'))
+		el.innerHTML = `<p>${message}</p>`
+	}
 })
 
 
